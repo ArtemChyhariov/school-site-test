c00b85176f2813d1ce53d65ed37e51affe6b5110
Your Name <you@example.com>
2024-11-21 20:50:34 +0200

    server atemp 1

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('hello world!');
+import express from 'express'
+
+const app = express()
+
+
+app.get('/', async (req, res) => {
+    res.send('is working!!!')
+})
+
+app.listen(3000, () => {
+    console.log('server started.');
+})
+
